f1ce4c66e3796b5dee3c3fc40776607d3eb3509c
Julian Todd <julian@goatchurch.org.uk>
2021-09-23 16:13:23 +0000

    does this flake thing work

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      # Generate a user-friendly version numer.
+      version = builtins.substring 0 8 self.lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+    {
+      overlay = final: prev: {};
+
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor."${system}";
+        in
+          pkgs.mkShell {
+            buildInputs = with pkgs.python39Packages; [
+              jupyter scipy matplotlib pandas matplotlib sympy ezdxf nurbs
+            ];
+          }
+      );
+
+    };
+  
+}
